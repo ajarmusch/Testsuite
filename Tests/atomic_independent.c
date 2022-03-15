fb41b89db84cd5849f335daac92a65d584ecebee
ajarmusch <jarmusch@udel.edu>
2022-03-15 11:56:55 -0400

    atomic independent

--- Tests/atomic_independent.c
+++ Tests/atomic_independent.c
@@ -37,6 +37,40 @@ int test1(){
 }
 #endif
 
+#ifndef T2
+//T2:parallel,loop,combined-constructs,V:2.0-2.7
+int test2(){
+    int err = 0;
+    srand(SEED);
+    real_t * a = (real_t *)malloc(n * sizeof(real_t));
+    real_t * a_copy = (real_t *)malloc(n * sizeof(real_t));
+    real_t * b = (real_t *)malloc(n * sizeof(real_t));
+
+    for (int x = 0; x < n; ++x){
+        a[x] = rand() / (real_t)(RAND_MAX / 10);
+        a_copy[x] = a[x];
+    }
+
+    #pragma acc data copy(a[0:n])
+    {
+      #pragma acc parallel loop auto
+      for (int x = 1; x < n; ++x){
+        a[x] = a[x - 1] + a[x];
+      }
+    }
+
+    real_t rolling_total = 0.0;
+    for (int x = 0; x < n; ++x){
+      rolling_total += a_copy[x];
+      if (fabs(rolling_total - a[x]) > PRECISION){
+        err = 1;
+      }
+    }
+
+    return err;
+}
+#endif
+
 int main(){
     int failcode = 0;
     int failed;
@@ -47,6 +81,15 @@ int main(){
     }
     if (failed != 0){
         failcode = failcode + (1 << 0);
+    }
+#endif
+#ifndef T2
+    failed = 0;
+    for (int x = 0; x < NUM_TEST_CALLS; ++x){
+        failed = failed + test2();
+    }
+    if (failed != 0){
+        failcode = failcode + (1 << 1);
     }
 #endif
     return failcode;
