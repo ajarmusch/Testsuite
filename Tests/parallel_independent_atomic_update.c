8e9a10f0f65de11e95b61d0f9f400e517c96cdc1
ajarmusch <jarmusch@udel.edu>
2022-03-16 19:17:17 -0400

    piaur

--- Tests/parallel_independent_atomic_update.c
+++ Tests/parallel_independent_atomic_update.c
@@ -12,51 +12,14 @@ int test1(){
         b[x] = rand() / (real_t)(RAND_MAX / 10);
     }
 
-    #pragma acc data copy(a[0:n], b[0:n])
+    #pragma acc data copy(a[0:n])
     {
         #pragma acc parallel
         {
 	    #pragma acc loop independent
             for (int x = 0; x < n; ++x){
                 #pragma acc atomic update
-                    a[x] = a[x] * 2; //dependent
-                    b[x] = a[x];
-            }
-        }
-    }
-
-    for (int x = 0; x < n; ++x){
-        if (fabs(a[x] - b[x]) > PRECISION){
-            err += 1;
-        }
-    }
-
-    return err;
-}
-#endif
-
-#ifndef T2
-//T2:parallel,loop,V:2.7-3.2
-int test2(){
-    int err = 0;
-    srand(SEED);
-    real_t *a = (real_t *)malloc(n * sizeof(real_t));
-    real_t *b = (real_t *)malloc(n * sizeof(real_t));
-
-    for (int x = 0; x < n; ++x){
-        a[x] = rand() / (real_t)(RAND_MAX / 10);
-        b[x] = rand() / (real_t)(RAND_MAX / 10);
-    }
-
-    #pragma acc data copy(a[0:n], b[0:n])
-    {
-        #pragma acc parallel
-        {
-	    #pragma acc loop independent
-            for (int x = 0; x < n; ++x){
-                #pragma acc atomic update
-                    a[x] = a[x] * 2; //independent
-                    b[x] = a[x];
+                    (a[x])++;
             }
         }
     }
@@ -82,15 +45,6 @@ int main(){
    if (failed != 0){
        failcode = failcode + (1 << 0);
    }
-#endif
-#ifndef T2
-    failed = 0;
-    for (int x = 0; x < NUM_TEST_CALLS; ++x){
-        failed = failed + test2();
-    }
-    if (failed != 0){
-        failcode = failcode + (1 << 1);
-    }
 #endif
     return failcode;
 }
