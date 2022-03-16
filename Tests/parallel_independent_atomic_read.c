56d685a59f0b6dba7502062528972ee5bdc59f06
ajarmusch <jarmusch@udel.edu>
2022-03-16 18:54:03 -0400

    piac

--- Tests/parallel_independent_atomic_read.c
+++ Tests/parallel_independent_atomic_read.c
@@ -18,9 +18,8 @@ int test1(){
         {
 	    #pragma acc loop independent
             for (int x = 0; x < n; ++x){
-                #pragma acc atomic //capture
-                    a[x] = a[x] * 2;
-                    b[x] = a[x];
+                #pragma acc atomic write
+                    b[x] = a[x] * 2;
             }
         }
     }
@@ -31,40 +30,6 @@ int test1(){
         }
     }
 
-    return err;
-}
-#endif
-
-#ifndef T2
-//T2:parallel,loop,V:2.7-3.2
-int test2(){
-    int err = 0;
-    srand(SEED);
-    real_t * a = (real_t *)malloc(n * sizeof(real_t));
-    real_t * b = (real_t *)malloc(n * sizeof(real_t));
-
-    for (int x = 0; x < n; ++x){
-        a[x] = rand() / (real_t)(RAND_MAX / 10);
-        b[x] = 0;
-    }
-
-    #pragma acc data copy(a[0:n], b[0:n])
-    {
-        #pragma acc parallel 
-        {
-            #pragma acc loop independent
-                for (int x = 0; x < n; ++x){
-                    // #pragma acc atomic
-                        a[x] = b[x];
-                }
-        }
-    }
-
-    for (int x = 0; x < n; ++x){
-        if (fabs(a[x] - b[x]) > PRECISION){
-            err = 1;
-        }
-    }
     return err;
 }
 #endif
@@ -80,15 +45,6 @@ int main(){
    if (failed != 0){
        failcode = failcode + (1 << 0);
    }
-#endif
-#ifndef T2
-    failed = 0;
-    for (int x = 0; x < NUM_TEST_CALLS; ++x){
-        failed = failed + test2();
-    }
-    if (failed != 0){
-        failcode = failcode + (1 << 1);
-    }
 #endif
     return failcode;
 }
