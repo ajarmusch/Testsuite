27a9158c34d45f4c6ef0c9f5a79343acde57b1d4
ajarmusch <jarmusch@udel.edu>
2022-06-22 15:46:21 -0400

    PIA error fix

--- Tests/completed/parallel_independent_atomic.c
+++ Tests/completed/parallel_independent_atomic.c
@@ -55,8 +55,7 @@ int test2(){
 	    #pragma acc loop independent
             for (int x = 0; x < n; ++x){
                 #pragma acc atomic update
-                    a[x] = a[x] * 2; //independent
-                    b[x] = a[x];
+                    b[x] = a[x];   //independent
             }
         }
     }
